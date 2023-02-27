6597e22e7f135e3e89e46244d1c208ddef9982f3
stevy <stevyralambomanana@gmail.com>
2023-02-27 15:15:01 +0300

    reformat

--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.layout')
 
 @section('style')
-  
 @endsection
 
 @section('body')
-  <form action="{{route('profile.update',['locale' => session('locale')])}}" method="post">
-    @csrf
-    <div class="mx-auto" style="width: 50%;">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Nom</label>
-          <input type="text" name="name" value="{{$user->name}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <form action="{{ route('profile.update', ['locale' => session('locale')]) }}" method="post">
+        @csrf
+        <div class="mx-auto" style="width: 50%;">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Nom</label>
+                <input type="text" name="name" value="{{ $user->name }}" class="form-control" id="exampleInputEmail1"
+                    aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Prénom</label>
+                <input type="text" name="surname" value="{{ $user->name }}" class="form-control"
+                    id="exampleInputPassword1">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Adresse email</label>
+                <input type="email" name="email" value="{{ $user->email }}" class="form-control"
+                    id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <button type="submit" class="btn btn-primary">Enregistrer</button>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Prénom</label>
-          <input type="text" name="surname" value="{{$user->name}}" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Adresse email</label>
-          <input type="email" name="email" value="{{$user->email}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <button type="submit" class="btn btn-primary">Enregistrer</button>
-    </div>
-  </form>
+    </form>
 @endsection
 @section('script')
-<script>
-    
-</script>
+    <script></script>
 @endsection
