db1d67e744869103972a8538a8fa1758cfba6d35
stevy <stevyralambomanana@gmail.com>
2023-03-03 13:54:17 +0300

    debug cart

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,6 +17,12 @@ class Product extends Model
         if (Auth::guard('web')->check()) {
             $user = Auth::guard('web')->user();
             $detail = DetailPayment::where('user_id',$user->id)->first();
+            if (!isset($detail)) {
+                $detail = DetailPayment::create([
+                    'user_id' => $user->id,
+                    'total' => 0
+                ]);
+            }
             $exist = $detail->commands()->where('product_id',$this->id)->first();
             if (isset($exist)) {
                 return true ;
