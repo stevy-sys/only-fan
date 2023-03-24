3294cf42b5bb7ce2dc428d3842005abc7944c884
stevy <stevyralambomanana@gmail.com>
2023-03-24 23:58:29 +0300

    delete id app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     
 
 
-    <div id="app">
+    <div>
         {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
