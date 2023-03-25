1913b3baf0d2ba4984d63c50c31690961fa3b2a1
stevy <stevyralambomanana@gmail.com>
2023-03-25 13:55:23 +0300

    deconnexion

--- resources/views/layouts/layout_front.blade.php
+++ resources/views/layouts/layout_front.blade.php
@@ -290,32 +290,30 @@
   <script src="{{asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
   <script src="{{asset('assets/vendor/typed.js/typed.min.js')}}"></script>
   <script src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"
-            integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-            <script>
-              $(document).ready(function() {
-                  $("#chat-icon").click(function() {
-                      $("#chat-bubble").toggle();
-                  });
-                  $("#close-chat").click(function() {
-                      $("#chat-bubble").hide();
-                  });
-      
-                  $.ajaxSetup({
-                      headers: {
-                          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                      }
-                  });
-      
-                  $('#logout').click(function(e) {
-                      e.preventDefault();
-                      $.post("{{ route('logout',['locale' => session('locale')]) }}", function(data) {
-                          window.location.href = "/";
-                      });
-                  });
-              });
-          </script>
+  <script src=" {{asset('admin/vendor/jquery/jquery.min.js')}}"></script>
+      <script>
+        $(document).ready(function() {
+            $("#chat-icon").click(function() {
+                $("#chat-bubble").toggle();
+            });
+            $("#close-chat").click(function() {
+                $("#chat-bubble").hide();
+            });
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            $('#logout').click(function(e) {
+                e.preventDefault();
+                $.post("{{ route('logout',['locale' => session('locale')]) }}", function(data) {
+                    window.location.href = "/";
+                });
+            });
+        });
+    </script>
 
   <!-- Template Main JS File -->
   <script src="{{asset('assets/js/main.js')}}"></script>
