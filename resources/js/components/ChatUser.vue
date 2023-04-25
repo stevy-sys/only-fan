1f24f6e2ce9c31ade8dd92376eb2024913701903
Stephah <stephahnyah377@gmail.com>
2023-04-25 16:31:25 +0300

    chat integration

--- resources/js/components/ChatUser.vue
+++ resources/js/components/ChatUser.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mt-5">
+    <!-- <div class="container mt-5">
         <div class="row">
             <div class="col-md-6 col-lg-6 offset-md-3">
                 <div class="card">
@@ -11,7 +11,6 @@
                             <div v-for="(message, index) in myConversation.messages" :key="index">
                                 <li class="list-group-item" :class="message.user_id != auth ? 'sender-message' : 'receiver-message'">
                                     <div class="d-flex align-items-start">
-                                        <!-- <img src="https://via.placeholder.com/50x50" class="rounded-circle mr-3">  -->
                                         <div class="text-black p-2">
                                             <strong>{{ message.user.name}}</strong>
                                             <p class="text-muted">{{ message.created_at }}</p>
@@ -23,7 +22,6 @@
                         </ul>
                     </div>
                     <div class="card-footer">
-                        <!-- /chat -->
                         <form @submit.prevent="sendMessage()" method="post">
                             <div class="input-group">
                                 <input type="text" v-model="form.message" class="form-control" name="message" id="message-input">
@@ -36,10 +34,54 @@
                 </div>
             </div>
         </div>
+    </div> -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-10">
+                <div class="" style="height: calc(100vh - 56px);">
+                    <div class="container">
+                        <div class="row">
+                            <!-- <div class="col-md-4">
+                                <ul class="list-group">
+                                    <a @click="changeConversation(conversation)" v-for="(conversation, index) in conversations" :key="index" class="mb-2" href="#">
+                                        <li class="list-group-item active">
+                                            {{conversation.talked.user.name}}
+                                        </li>
+                                    </a>
+                                </ul>
+                            </div> -->
+                            <div class="col-md-8">
+                                <div class="card">
+                                    <!-- <div class="card-header">{{conversationActive.talked.user.name}}</div> -->
+                                    <div class="card-body" ref="messageList">
+                                        <div v-for="(message, i) in myConversation.messages" :key="i">
+                                            <div class="message" :class="message.user_id == auth ? 'moi' : 'toi'">
+                                                <p>{{message.message}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer">
+                                        <form @submit.prevent="sendMessage()" method="post">
+                                            <div class="input-group">
+                                                <input v-model="form.message" type="text" name="message" class="form-control"
+                                                    placeholder="Votre message">
+                                                <button type="submit" class="btn btn-primary">Envoyer</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+// <!-- <img src="https://via.placeholder.com/50x50" class="rounded-circle mr-3">  -->
     export default {
         name:"ChatUser",
         props:['conversation','auth'],
@@ -64,9 +106,8 @@
                         this.myConversation.messages.push(res.data.message) 
                         this.form.message = ""
                         this.$nextTick(() => {
-                            const chatBody = this.$refs.chatBody;
-                            chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: 'smooth' });
-                        })
+                            this.$refs.messageList.lastChild.scrollIntoView({ behavior: 'smooth' });
+                        });
                     }
                 })
                 .catch((err) => {
@@ -79,14 +120,12 @@
             window.Echo.private(`ChannelChat.${this.auth}`).listen("ChatChannel", ({ data }) => {
                 this.myConversation.messages.push(data)
                 this.$nextTick(() => {
-                    const chatBody = this.$refs.chatBody;
-                    chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: 'smooth' });
-                })
+                    this.$refs.messageList.lastChild.scrollIntoView({ behavior: 'smooth' });
+                });
             });
             this.$nextTick(() => {
-                const chatBody = this.$refs.chatBody;
-                chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: 'smooth' });
-            })
+                this.$refs.messageList.lastChild.scrollIntoView({ behavior: 'smooth' });
+            });
         }
     }
 </script>
--- resources/views/user/chat/index.blade.php
+++ resources/views/user/chat/index.blade.php
@@ -1,9 +1,30 @@
 @extends('layouts.layout_front')
 
 @section('style')
-    <style>
-
-    </style>
+<style>
+    .card-body {
+        overflow-y: auto; max-height: 400px;
+    }
+    .moi {
+        display: flex !important;
+        justify-content: end !important;
+        border: 1px #4e73df solid;
+        padding: 5px;
+        border-radius: 12px;
+        color: black;
+        background-color: #4e73df;
+    }
+    .toi{
+        border: 1px #e3e6f0 solid;
+        padding: 5px;
+        border-radius: 12px;
+        color: black;
+        background-color: #e3e6f0;
+    }
+    .message{
+        margin-bottom: 10px
+    }
+</style>
 @endsection
 
 @section('sub_title')
